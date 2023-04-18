625312bca0d3753cc68acbef10c188c365182b1a
Asus <aliendev.uz998>
2023-04-18 22:34:13 +0500

    LoadingButtonAndroid kutubxonasidan foydalanildi

--- app/src/main/java/com/example/localizationjava/activity/LanguageActivity.java
+++ app/src/main/java/com/example/localizationjava/activity/LanguageActivity.java
@@ -1,16 +1,18 @@
 package com.example.localizationjava.activity;
 
-import android.content.res.Configuration;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.os.LocaleListCompat;
 
+import com.example.localizationjava.R;
 import com.example.localizationjava.databinding.ActivityLanguageBinding;
 
-import java.util.Locale;
-
-import timber.log.Timber;
+import br.com.simplepass.loadingbutton.customViews.CircularProgressButton;
 
 public class LanguageActivity extends AppCompatActivity {
 
@@ -24,20 +26,53 @@ public class LanguageActivity extends AppCompatActivity {
         init();
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     void init() {
-        binding.buttonChinese.setOnClickListener(v -> setLocale("zh"));
-        binding.buttonKorean.setOnClickListener(v -> setLocale("ko"));
-        binding.buttonJapanese.setOnClickListener(v -> setLocale("ja"));
-        binding.buttonUzbek.setOnClickListener(v -> setLocale("uz"));
-        Timber.d("Salom");
+        CircularProgressButton buttonChinese = binding.buttonChinese;
+        CircularProgressButton buttonKorean = binding.buttonKorean;
+        CircularProgressButton buttonJapanese = binding.buttonJapanese;
+        CircularProgressButton buttonUzbek = binding.buttonUzbek;
+
+        buttonChinese.setOnClickListener(v -> {
+            buttonChinese.startAnimation();
+            new Handler().postDelayed(() -> {
+                buttonChinese.revertAnimation();
+                buttonChinese.setBackgroundDrawable(getDrawable(R.drawable.circular_border_shape));
+                setLocale("zh");
+            }, 400L);
+        });
+
+        buttonKorean.setOnClickListener(v -> {
+            buttonKorean.startAnimation();
+            new Handler().postDelayed(() -> {
+                buttonKorean.revertAnimation();
+                buttonKorean.setBackgroundDrawable(getDrawable(R.drawable.circular_border_shape));
+                setLocale("ko");
+            }, 400L);
+        });
+
+        buttonJapanese.setOnClickListener(v -> {
+            buttonJapanese.startAnimation();
+            new Handler().postDelayed(() -> {
+                buttonJapanese.revertAnimation();
+                buttonJapanese.setBackgroundDrawable(getDrawable(R.drawable.circular_border_shape));
+                setLocale("ja");
+            }, 400L);
+        });
+
+        buttonUzbek.setOnClickListener(v -> {
+            buttonUzbek.startAnimation();
+            new Handler().postDelayed(() -> {
+                buttonUzbek.revertAnimation();
+                buttonUzbek.setBackgroundDrawable(getDrawable(R.drawable.circular_border_shape));
+                setLocale("uz");
+            }, 400L);
+        });
     }
 
     void setLocale(String lan) {
-        Locale locale = new Locale(lan);
-        Locale.setDefault(locale);
-        Configuration config = getResources().getConfiguration();
-        config.setLocale(locale);
-        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        LocaleListCompat appLocale = LocaleListCompat.forLanguageTags(lan);
+        AppCompatDelegate.setApplicationLocales(appLocale);
         finish();
     }
 }
--- app/src/main/java/com/example/localizationjava/activity/MainActivity.java
+++ app/src/main/java/com/example/localizationjava/activity/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.localizationjava.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.localizationjava.R;
 import com.example.localizationjava.databinding.ActivityMainBinding;
 
+import br.com.simplepass.loadingbutton.customViews.CircularProgressButton;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,11 +29,17 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     void init() {
-        Button buttonOpen = binding.buttonOpen;
+        CircularProgressButton buttonOpen = binding.buttonOpen;
         buttonOpen.setOnClickListener(v -> {
-            Intent intent = new Intent(context, LanguageActivity.class);
-            startActivity(intent);
+            buttonOpen.startAnimation();
+            new Handler().postDelayed( () -> {
+                buttonOpen.revertAnimation();
+                buttonOpen.setBackgroundDrawable(getDrawable(R.drawable.circular_border_shape));
+                Intent intent = new Intent(context, LanguageActivity.class);
+                startActivity(intent);
+            }, 1400L);
         });
     }
 }
